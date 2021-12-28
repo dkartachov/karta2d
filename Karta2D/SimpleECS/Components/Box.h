0b45231b9580fb65e197f0a8b0737e574db96048
Denis Kartachov <kartachovd@gmail.com>
2021-12-27 22:24:42 -0500

    remove SDL_Rect

--- Karta2D/SimpleECS/Components/Box.h
+++ Karta2D/SimpleECS/Components/Box.h
@@ -6,12 +6,8 @@ class Box : public Component {
 public:
 	Box() {
 		graphics = Graphics::Instance();
-
-		color = { 255, 255, 255, 255 };
 		size = 50 * oneVector;
-		rect.w = size.x;
-		rect.h = size.y;
-		rect.x = rect.y = 0;
+		color = { 255, 255, 255, 255 };
 	}
 
 	void init() override {
@@ -39,10 +35,7 @@ public:
 	}
 
 	void update() override {
-		rect.x = (int)transform->getPosition().x - rect.w / 2;
-		rect.y = (int)transform->getPosition().y - rect.h / 2;
-		rect.w = (int)size.x * transform->getScale().x;
-		rect.h = (int)size.y * transform->getScale().y;
+		
 	}
 
 	void render() override {
@@ -58,7 +51,6 @@ public:
 private:
 	Graphics* graphics;
 	Transform2D* transform;
-	SDL_Rect rect;
-	SDL_Color color;
 	Vector2D size;
+	SDL_Color color;
 };
