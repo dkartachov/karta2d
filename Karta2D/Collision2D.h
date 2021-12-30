5deeeafb93627f3e4fbe9633a4848dc5cf0864c2
Denis Kartachov <kartachovd@gmail.com>
2021-12-30 10:18:19 -0500

    remove repeated code

--- Karta2D/Collision2D.h
+++ Karta2D/Collision2D.h
@@ -5,13 +5,16 @@
 class Collision2D {
 public:
 	static bool AABB(Entity& ent1, Entity& ent2) {
-		BoxCollider2D ent1BoxCol = *ent1.GetComponent<BoxCollider2D>();
-		BoxCollider2D ent2BoxCol = *ent2.GetComponent<BoxCollider2D>();
+		Vector2D ent1Position = ent1.GetComponent<Transform2D>()->getPosition();
+		Vector2D ent1Box = ent1.GetComponent<BoxCollider2D>()->getSize();
 
-		if (ent1.GetComponent<Transform2D>()->getPosition().x + 0.5 * ent1BoxCol.getSize().x > ent2.GetComponent<Transform2D>()->getPosition().x - 0.5 * ent2BoxCol.getSize().x &&
-			ent1.GetComponent<Transform2D>()->getPosition().x - 0.5 * ent1BoxCol.getSize().x < ent2.GetComponent<Transform2D>()->getPosition().x + 0.5 * ent2BoxCol.getSize().x &&
-			ent1.GetComponent<Transform2D>()->getPosition().y + 0.5 * ent1BoxCol.getSize().y > ent2.GetComponent<Transform2D>()->getPosition().y - 0.5 * ent2BoxCol.getSize().y &&
-			ent1.GetComponent<Transform2D>()->getPosition().y - 0.5 * ent1BoxCol.getSize().y < ent2.GetComponent<Transform2D>()->getPosition().y + 0.5 * ent2BoxCol.getSize().y) {
+		Vector2D ent2Position = ent2.GetComponent<Transform2D>()->getPosition();
+		Vector2D ent2Box = ent2.GetComponent<BoxCollider2D>()->getSize();
+
+		if (ent1Position.x + 0.5 * ent1Box.x > ent2Position.x - 0.5 * ent2Box.x &&
+			ent1Position.x - 0.5 * ent1Box.x < ent2Position.x + 0.5 * ent2Box.x &&
+			ent1Position.y + 0.5 * ent1Box.y > ent2Position.y - 0.5 * ent2Box.y &&
+			ent1Position.y - 0.5 * ent1Box.y < ent2Position.y + 0.5 * ent2Box.y) {
 
 			return true;
 		}
@@ -20,8 +23,10 @@ public:
 	}
 
 	static void resolveAABBCollisions() {
-		for (auto& thisEntity : EntityManager::getInstance().getEntities()) {
-			for (auto& entity : EntityManager::getInstance().getEntities()) {
+		std::vector<Entity*>& entities = EntityManager::getInstance().getEntities();
+
+		for (auto& thisEntity : entities) {
+			for (auto& entity : entities) {
 				if (thisEntity->getId() != entity->getId()) {
 					if (AABB(*thisEntity, *entity)) {
 						std::printf("Collision detected between entities %d and %d\n", thisEntity->getId(), entity->getId());
